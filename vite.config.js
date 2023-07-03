71d8bc822e917a1285b1c6d173e879ea64d1b026
Zoran <pujic.zoran@gmail.com>
2023-07-03 19:55:23 +0200

    add react scoped css dependency

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, createLogger } from 'vite';
+import react from '@vitejs/plugin-react';
+import { reactScopedCssPlugin } from 'rollup-plugin-react-scoped-css';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  const logger = createLogger();
+
+  return {
+    customLogger: logger,
+    plugins: [
+      react(),
+      reactScopedCssPlugin(),
+    ],
+  };
+});
