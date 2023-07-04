fc5a97bd8df518bc35357c9b7daec554a31b557e
Zoran <pujic.zoran@gmail.com>
2023-07-04 23:04:52 +0200

    add data variable

---
+++ src/constants/seed-data.js
@@ -0,0 +1,38 @@
+import Spencer from '../assets/Spencer.png';
+import Jayd from '../assets/Jay-d.png';
+import Nibble from '../assets/Nibble.png';
+
+const SEED_DATA = [
+  {
+    image: Spencer,
+    title: 'Spencer',
+    description: 'A DIY voice assistant that talks, lights up, and understands voice commands',
+    price: '1,027.99',
+    discount: '0.20',
+    button: 'Shop now',
+    ages: '11',
+    color: '#e3384d',
+  },
+  {
+    image: Jayd,
+    title: 'Jay-D',
+    description: 'Build & code your own DJ mixer',
+    price: '1,027.99',
+    discount: '0.15',
+    button: 'Shop now',
+    ages: '11',
+    color: '#fed600',
+  },
+  {
+    image: Nibble,
+    title: 'Nibble',
+    description: 'Build & code your very own retro game console',
+    price: '813.99',
+    discount: '0.2',
+    button: 'Shop now',
+    ages: '9',
+    color: '#00bed6',
+  },
+];
+
+export default SEED_DATA;
